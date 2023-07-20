209a77338e81b77fc38b77b3d9f0d78e48d6ca9d
Alejandro <alejandroaperez1994g@gmail.com>
2023-07-20 09:35:56 +0200

    :sparkles: added cloudinary upload files feature

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,33 @@
 import '../App.css'
 import {useAuth0} from "@auth0/auth0-react";
-import {protectedRequest, publicRequest} from "../services/request.service.ts";
+import {protectedRequest, uploadRequest} from "../services/request.service.ts";
+import React, {useState} from "react";
 
 function App() {
     const { logout, getAccessTokenSilently } = useAuth0()
-  return (
-    <>
+    const [file, setFile] = useState<File>()
 
-      <h1>Vite + React + Home</h1>
-      <div className="card">
-        <button onClick={()=>publicRequest()}>Public Request</button>
-        <button onClick={()=>protectedRequest(getAccessTokenSilently)}>Protected Request</button>
-        <button onClick={()=>logout()}>Logout</button>
-      </div>
-    </>
-  )
+    const handleFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if(e.target.files !== null){
+            const file = e.target.files[0];
+            setFile(file);
+            console.log(file)
+        }
+    }
+
+
+    return (
+        <>
+
+            <h1>Vite + React + Home</h1>
+            <div className="card">
+                <input  accept='image/*,audio/*' type="file" onChange={handleFileInput}/>
+                <button onClick={()=>uploadRequest(getAccessTokenSilently,file)}>Upload File Request</button>
+                <button onClick={()=>protectedRequest(getAccessTokenSilently)}>Protected Request</button>
+                <button onClick={()=>logout()}>Logout</button>
+            </div>
+        </>
+    )
 }
 
 export default App
--- src/services/request.service.ts
+++ src/services/request.service.ts
@@ -1,3 +1,7 @@
+import {GetTokenSilentlyOptions} from "@auth0/auth0-react";
+
+type GetAccessTokenFunction = (options?: GetTokenSilentlyOptions) => Promise<string | undefined>
+
 export const publicRequest = async () => {
     const {VITE_API_URL: url} = import.meta.env
     const response = await fetch(`${url}/public`);
@@ -5,13 +9,28 @@ export const publicRequest = async () => {
     console.log(data);
 }
 
-export const protectedRequest = async (getToken: any) => {
+export const protectedRequest = async (getToken: GetAccessTokenFunction) => {
     const {VITE_API_URL: url} = import.meta.env
     const token = await getToken();
     const response = await fetch(`${url}/protected`, {
         headers: {
             authorization: `Bearer ${token}`
         }
+    });
+    const data = await response.json();
+    console.log(data);
+}
+export const uploadRequest = async (getToken: GetAccessTokenFunction, file: File | undefined) => {
+    const {VITE_API_URL: url} = import.meta.env
+    const token = await getToken();
+    const formData = new FormData();
+    file && formData.append("image", file);
+    const response = await fetch(`${url}/upload`, {
+        method: "POST",
+        headers: {
+            authorization: `Bearer ${token}`
+        },
+        body: formData
     });
     const data = await response.json();
     console.log(data);
